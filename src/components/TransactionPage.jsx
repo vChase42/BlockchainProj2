26bbcd59905434603b437c34f30734ff3e6d69fd
David Wang <david.wang5766@gmail.com>
2022-04-20 13:37:49 -0700

    added comments

--- src/components/TransactionPage.jsx
+++ src/components/TransactionPage.jsx
@@ -6,8 +6,23 @@ import TransactionTable from "./TransactionTable";
 const TransactionPage = (props) => {
     const [watchAddress, setWatchAddress] = useState("0x3057471208FEe8D6a079ae7664B89211497A4883");
     
-
     
+
+
+    //PLACEHOLDER: call the 'getEthTransactions' cloud function
+
+    //PLACEHOLDER: setup subscription to track sync status of current address
+
+    //PLACEHOLDER: refetch data when data is synced
+
+
+    //helper function to extract the boolean indicating whether watchAddress is still syncing or not from isSyncing
+    const syncingToSynced = (arg) => {
+        if (arg?.at(0)?.attributes?.is_syncing === false){
+            return false;
+        }
+        return true;
+    }
 
     //helper function to extract array of transactions from the object that 'getEthTransactions' returns
     const postProcess = (raw) => {
